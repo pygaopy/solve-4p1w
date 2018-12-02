df981c0392d5daa369a6827a378acfdb640a9ac5
pygaopy <45216773+pygaopy@users.noreply.github.com>
2018-12-02 19:08:43 +0800

    Create solve-4p1w.py
    first the idea, then one by one, realize it.

---
+++ solve-4p1w.py
@@ -0,0 +1,7 @@
+# try my best to think about 4p1w
+""" first, specify how many letters in the word.
+then select how many letter options, remove repetiton.
+then search dictionary
+then output the final list of all combinations.
+
+"""
